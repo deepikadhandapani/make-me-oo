ad6441c89366c18ad2811eb26b49a75f091e1ba9
Deepi <d.deepikadeepi1999@gmail.com>
2020-10-18 18:42:09 +0530

    [Deepika] Removed DistanceAndDirectionCalculator class and Added interface

---
+++ src/main/java/org/oop/DistanceAndDirection.java
@@ -0,0 +1,7 @@
+package org.oop;
+
+interface DistanceAndDirection{
+    double distance(Point point);
+    double direction(Point point);
+}
+
--- src/main/java/org/oop/Point.java
+++ src/main/java/org/oop/Point.java
@@ -1,27 +1,22 @@
 package org.oop;
 
-public class Point {
+public class Point implements DistanceAndDirection {
     private double x;
     private double y;
-
-    public double getX() {
-        return x;
-    }
-
-    public void setX(double x) {
-        this.x = x;
-    }
-
-    public double getY() {
-        return y;
-    }
-
-    public void setY(double y) {
-        this.y = y;
-    }
-
     public Point(double x, double y) {
         this.x = x;
         this.y = y;
     }
+    public double distance(Point to) {
+        double xDistance = to.x - this.x;
+        double yDistance = to.y - this.y;
+        return Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2));
+    }
+
+    public double direction( Point to) {
+        double xDistance = to.x - this.x;
+        double yDistance = to.y - this.y;
+        return Math.atan2(yDistance, xDistance);
+    }
+
 }
